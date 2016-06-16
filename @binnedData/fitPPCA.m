cabcf1ffe098fe9c23f0daeb9dd6df51c59da17c
Christopher Steven Versteeg <csv057@fsm.northwestern.edu>
2016-06-16 10:47:29 -0500

    added kernSD functionality to smooth data if required, also commented code for ease of use

--- @binnedData/fitPPCA.m
+++ @binnedData/fitPPCA.m
@@ -1,17 +1,30 @@
 function fitPPCA(binned)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
- %this is a method function of the binnedData class and should be saved
-    %in the @binnedData folder with the class definition and other methods
-    %files
-    %
-    %bd.fitPds uses the configuration in the bd.pdConfig field to compute
-    %preferred directions for each unit and stores the result in the
-    %bd.pdData field
-    %get our list of units
+%function fitPPCA
+%       inputs:
+%            binned data structure with ppcaConfig filled with needed
+%            parameters
+%       Outputs:
+%           First n factors of data set specified in config file. Saved in
+%           ppcaData section of data structure
+%       Config Structure:
+%
+%           windows : start and end times of windows to perform
+%           dimensionality reduction on (eg. [targetAppearsTrial1,
+%           goCueTrial1;targetAppearsTrial2, goCueTrial2....]
+%           
+%           dimension: dimension of reduced dataset
+%
+%           segLength: Lenght of smaller segments to cut trials into. Makes
+%           computation of factors faster for FA and GPFA if trials are of
+%           equal length
+%
+%           trials: trial numbers corresponding to windows
+%
+%           kernSD: smoothing kernel standard deviation. Larger value acts
+%           as a larger low pass filter on trajectories
     method = 'ppca';
     dat = dimRedHelper(binned, method);
-    kernSD = 10;
+    kernSD = binned.ppcaConfig.kernSD;
     runIdx =20;
     xDim = binned.ppcaConfig.dimension;
     result = neuralTraj(runIdx,dat, 'method', method, 'xDim', xDim, 'kernSDList', kernSD, 'segLength', binned.gpfaConfig.segLength);
