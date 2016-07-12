f653ca4c3d2b809210c2971305f3df6f9d5013f7
bdek <brian.dekleva@gmail.com>
2016-07-12 18:39:03 -0500

    added getTT file for Sabes task and modified the one for UNT

--- @commonDataStructure/getSABESTaskTable.m
+++ @commonDataStructure/getSABESTaskTable.m
@@ -1,4 +1,4 @@
-function getUNTTaskTable(cds,times)
+function getSABESTaskTable(cds,times)
     %this is a method of the commonDataStructure class and must be saved in
     %the @commonDataStructure folder with the other method definitions
     %
@@ -7,21 +7,24 @@ function getUNTTaskTable(cds,times)
     %ctrOnTime      -time the center target appeared
     %tgtOnTime      -time the ourter target appeared
     %goCueTime      -time of the go cue
+    %otHoldTime     -time that the outer hold starts
     %endTime        -time the trial ended
     %result         -result of the trial (RAFI)
     %tgtDir         -direction of the true target in degrees
-    %cuePrior       -kappe for the prior of the cue
-    %tgtPrior       -kappa for the prior of the actual targets
-    
+    %cursShift      -cursor offset from hand
+    %tgtShift       -visual shift
 
-    centerOnTime       = cds.words.ts(cds.words.word==hex2dec('30'));
+    centerOnTime    = cds.words.ts(cds.words.word==hex2dec('30'));
     otOnTime        = cds.words.ts(cds.words.word==hex2dec('40'));
     goCueTime       = cds.words.ts(cds.words.word==hex2dec('31'));
+    otHoldTime      = cds.words.ts(cds.words.word==hex2dec('A1'));
+    
     %preallocate vectors:
     numTrials=numel(times.number);
     tgtDirList=nan(numTrials,1);
-    tgtPriorList=nan(numTrials,1);
-    cuePriorList=nan(numTrials,1);
+    cursShiftList=nan(numTrials,1);
+    tgtShiftList=nan(numTrials,1);
+    holdTime=nan(numTrials,1);
     goTime=nan(numTrials,1);
     OTTime=nan(numTrials,1);
     ctrOnTime=nan(numTrials,1);
@@ -32,15 +35,19 @@ function getUNTTaskTable(cds,times)
         %get target and prior info from databurst
         if ~isempty(idxDB) 
             tgtDirList(trial) = 180*bytes2float(cds.databursts.db(idxDB,10:13))/pi;
-            tgtPriorList(trial) = bytes2float(cds.databursts.db(idxDB,14:17));
-            cuePriorList(trial)=bytes2float(cds.databursts.db(idxDB,18:21));
-            if cuePriorList > 100000 
-                cuePriorList(trial) = NaN;
-            end
+            cursShiftList(trial) = bytes2float(cds.databursts.db(idxDB,14:17));
+            tgtShiftList(trial)=bytes2float(cds.databursts.db(idxDB,18:21));
         else
             tgtDirList(trial) = NaN;
-            tgtPriorList(trial) = NaN;
-            cuePriorList(trial) = NaN;
+            cursShiftList(trial) = NaN;
+            tgtShiftList(trial) = NaN;
+        end
+        % get the timestamp for the outer hold
+        oHT = otHoldTime(find(otHoldTime<times.endTime(trial) & otHoldTime>times.startTime(trial),1,'first'));
+        if isempty(oHT)
+            holdTime(trial)=NaN;
+        else
+            holdTime(trial)=oHT;
         end
         % get the timestamp for the go cue
         gT = goCueTime(find(goCueTime<times.endTime(trial) & goCueTime>times.startTime(trial),1,'first'));
@@ -67,26 +74,26 @@ function getUNTTaskTable(cds,times)
 
     % Deal with weird prior databursts
     checkPrior = @(burst) burst < 10e-5 | burst > 1e5+1 | isnan(burst);
-    badBursts = find(checkPrior(tgtPriorList));
-    goodBursts = find(~checkPrior(tgtPriorList));
+    badBursts = find(checkPrior(cursShiftList));
+    goodBursts = find(~checkPrior(cursShiftList));
     for i = 1:length(badBursts)
         bb = badBursts(i);
         ind_dists = abs(goodBursts - bb);
         replacer_ind = goodBursts(find(ind_dists==min(ind_dists),1,'first'));
-        replacer = tgtPriorList(replacer_ind);
-        tgtPriorList(bb)= replacer;
+        replacer = cursShiftList(replacer_ind);
+        cursShiftList(bb)= replacer;
     end
 
-    trialsTable=table(roundTime(ctrOnTime,.001),roundTime(goTime,.001),roundTime(OTTime,.001),...
-                        tgtDirList,cuePriorList,tgtPriorList,...
-                        'VariableNames',{'ctrOnTime','goCueTime','tgtOnTime','tgtDir','cuePrior','tgtPrior'});
-    trialsTable.Properties.VariableUnits={'s','s','s','Deg','AU','AU'};
-    trialsTable.Properties.VariableDescriptions={'center target onset time','go cue time','outer target onset time',...
-                                                    'actual target direction','kappa of the von mises function for the visual cue','kappa for the von mises function for the actual target locations'};
+    trialsTable=table(roundTime(ctrOnTime,.001),roundTime(OTTime,.001),roundTime(goTime,.001),...
+                        roundTime(holdTime,.001),tgtDirList,cursShiftList,tgtShiftList,...
+                        'VariableNames',{'ctrOnTime','OTTime','goTime','holdTime','tgtDir','cursShift','tgtShift'});
+    trialsTable.Properties.VariableUnits={'s','s','s','s','Deg','cm','cm'};
+    trialsTable.Properties.VariableDescriptions={'center target onset time','outer target onset time','go cue time',...
+                                                 'outer target hold time','target angle','cursor shift','visual shift'};
     
     trialsTable=[times,trialsTable];
-    trialsTable.Properties.Description='Trial table for the UNT task';
+    trialsTable.Properties.Description='Trial table for the center-out Sabes task';
     set(cds,'trials',trialsTable)
-    evntData=loggingListenerEventData('getUNTTaskTable',[]);
+    evntData=loggingListenerEventData('getSABTaskTable',[]);
     notify(cds,'ranOperation',evntData)
 end
