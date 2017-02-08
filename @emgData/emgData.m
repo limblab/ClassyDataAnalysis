9ffd139c83e200691738a7c291d920142c1ef6d1
Raeed Hasan Chowdhury <raeed.chowdhury@gmail.com>
2017-02-08 14:34:56 -0600

    Add processDefault to emgData class to high pass, rectify, and low pass emg data

--- @emgData/emgData.m
+++ @emgData/emgData.m
@@ -9,9 +9,11 @@ classdef emgData < timeSeriesData
     end
     events
         rectified
+        processedDefault
     end
     methods (Static = false)
         %general methods
         rectify(emg)
+        processDefault(emg)
     end
 end
